880bfe930f0fa8b1cd52dd0545c6da0a07e6b71f
AndrewCheUA <andreicheban@gmail.com>
2023-08-19 02:35:34 +0300

    db update

--- ai_app.py
+++ ai_app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
+from app.database.models import UserProfile
+from app.database.connect import Session
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/form')
 def form():
     return render_template('form.html')
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -18,10 +23,16 @@ def submit():
     hold_back = request.form.get('hold_back')
     start_preference = request.form.get('start_preference')
 
-    # Process the data or save it to a database if needed
-    # ...
+    # Save data to the database
+    session = Session()
+    user_profile = UserProfile(name=name, preferred_hours=preferred_hours, hold_back=hold_back,
+                               start_preference=start_preference)
+    session.add(user_profile)
+    session.commit()
+    session.close()
 
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
